98ec5e9a28105624c80788bc780d66e68c4c6614
Tinsae Yared <tinsae.yb@gmail.com>
2023-05-31 13:37:02 -0500

    generic

--- src/main/java/com/example/lab/LabApplication.java
+++ src/main/java/com/example/lab/LabApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab1Application {
+public class LabApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Lab1Application.class, args);
+        SpringApplication.run(LabApplication.class, args);
     }
 
 }
--- src/test/java/com/example/lab/LabApplicationTests.java
+++ src/test/java/com/example/lab/LabApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab1ApplicationTests {
+class LabApplicationTests {
 
     @Test
     void contextLoads() {
